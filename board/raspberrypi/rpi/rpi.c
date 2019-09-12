17e979f042da947f741fd6a2128e9cab0e60663a
Florin Sarbu <florin@balena.io>
2019-09-12 12:31:31 +0200

    raspberrypi: Disable simple framebuffer support
    On 4.19 kernels this u-boot driver clashes with bcm2708_fb.
    So let's disable it from here so that we have bcm2708_fb
    enabled in the kernel, just as Raspbian users might expect.
    See https://github.com/raspberrypi/linux/issues/3139
    
    Upstream-Status: Inappropriate [configuration]
    Signed-off-by: Florin Sarbu <florin@balena.io>

--- board/raspberrypi/rpi/rpi.c
+++ board/raspberrypi/rpi/rpi.c
@@ -482,7 +482,7 @@ int ft_board_setup(void *blob, bd_t *bd)
 	 * should be more intelligent, and e.g. only do this if no enabled DT
 	 * node exists for the "real" graphics driver.
 	 */
-	lcd_dt_simplefb_add_node(blob);
+	//lcd_dt_simplefb_add_node(blob);
 
 #ifdef CONFIG_EFI_LOADER
 	/* Reserve the spin table */
